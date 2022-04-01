d3ead1d81359a717ca4ffd734820d824c48ec4b3
himanal <almassi.yazdan@gmail.com>
2022-04-01 15:22:26 +0430

    add server

--- src/context/Feedbackcontext.js
+++ src/context/Feedbackcontext.js
@@ -1,31 +1,49 @@
-import { useState, createContext } from "react";
-import Database from "../component/database/Database";
+import { useState, createContext, useEffect } from "react";
+// import Database from "../component/database/Database";
 import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
+
 
 const FeedbackContext= createContext()
 
 export const FeedbackProvider=({children})=>{
-    const [ feedback , setfeedback]= useState(Database)
-    const [ EditFeedback, setEditFeedback]= useState({
-        item:{},
-        edit:false
-    })
   
+  const [ feedback , setfeedback]= useState([])
+  const [ EditFeedback, setEditFeedback]= useState({
+    item:{},
+    edit:false
+  })
+  
+  useEffect(()=>{
+    fectData()
+  },[])
+  const fectData=async()=>{
+    const respone =await axios.get('http://localhost:4000/database')
+    let database = await respone.data
+     setfeedback(database)
+  }
 
-      const deleteFeedback =(id)=>{
+      const deleteFeedback =async (id)=>{
         if(window.confirm('are you sure')){
-    
-          setfeedback(
+            await axios.delete(`http://localhost:4000/database/${id}`)
+            setfeedback(
             feedback.filter((item)=> item.id !== id)
             )
           }
+
+
+        
       }
-      const addNewFeedback =(e)=>{
+      const addNewFeedback =async(e)=>{
+
         e.id=uuidv4()
+        await axios.post('http://localhost:4000/database',e)
         setfeedback([e, ...feedback])
     
        }
-       const upDateFeedback =( id , upDate)=>{
+       const upDateFeedback =async( id , upDate)=>{
+
+        const respone = await axios.put(`http://localhost:4000/database/${id}`,upDate)
         setfeedback(
           feedback.map((item)=>(
           item.id=== id ? {...item , ...upDate} : item)
